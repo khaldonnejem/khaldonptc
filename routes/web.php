279ed1a94f8dbd27bc0f77863cb1aa336b9748d9
khaldon <khaldon.r.n.n@gmail.com>
2023-02-28 11:22:51 +0200

    new commitj

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about',function(){
+    $name = 'khaldon';
+    // return view('about',['name' => $name]);
+    // return view('about')->with('name',$name);
+    return view('about',compact('name'));
+});
+
